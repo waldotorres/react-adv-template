011bc4440b2167893123c532291249d64a0ae546
Waldo Torres <waldotorres_82@hotmail.com>
2022-11-30 18:56:29 -0500

    control props alt - update

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -9,25 +9,6 @@ export const useShoppingCart = () => {
 
     setShoppingCart ( (oldValue:{ [key:string]:ProductInCart }) =>{
 
-      
-      //const productInCart:ProductInCart = oldValue[product.id] || { ...product, count:0 };
-
-
-      // if( Math.max( productInCart.count + count, 0 ) > 0 )
-      // {
-
-      //   productInCart.count += count
-
-      //   return {
-      //     ... oldValue,
-      //     [product.id]: productInCart
-      //   }
-
-      // }
-      // const{ [product.id]:toDelete, ...rest } = oldValue;
-      // return rest;
-
-
       if(count===0){
             // const otro = (Object.entries(oldValue).filter( e=> e[0] !== product.id ));
             // return Object.fromEntries(otro);
